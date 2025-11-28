523e3d4099faf439dbccf74160f417479487ed4e
Divakar <162953567+divakar9390@users.noreply.github.com>
2025-11-28 22:28:40 +0530

    Update Registration.js

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -33,7 +33,7 @@ function Registration() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/auth/register", {
+            const response = await fetch("https://smpa-backend-2.onrender.com/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
