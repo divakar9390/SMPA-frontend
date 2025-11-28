e35e01a0a021f0d2e936ecd88856a5b3dfed6db1
Divakar <162953567+divakar9390@users.noreply.github.com>
2025-11-28 22:27:57 +0530

    Update Login.js
    updated render url

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:8080/api/auth/login`, {
+      const response = await fetch(`https://smpa-backend-2.onrender.com/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(state),
